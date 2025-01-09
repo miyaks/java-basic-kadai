f76ca7000d9b45e3bc38951cc7850c3aa950e3a0
miyaks <105896227+miyaks@users.noreply.github.com>
2025-01-10 04:17:37 +0900

    課題提出

---
+++ kadai_021/src/kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,15 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+		Dictionary_Chapter21 dic = new Dictionary_Chapter21();
+		dic.tango();
+		
+		dic.search("apple");
+		dic.search("banana");
+		dic.search("grape");
+		dic.search("orange");
+	}
+
+}
