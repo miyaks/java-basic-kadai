c219818430a795d716e00cc46f2a392574ad658a
miyaks <105896227+miyaks@users.noreply.github.com>
2025-01-12 02:29:20 +0900

    課題再提出

--- kadai_021/src/kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/src/kadai_021/DictionaryExec_Chapter21.java
@@ -5,11 +5,9 @@ public class DictionaryExec_Chapter21 {
 	public static void main(String[] args) {
 		Dictionary_Chapter21 dic = new Dictionary_Chapter21();
 		dic.tango();
+		String[] search = new String[] {"apple","banana","grape","orange"};
+		dic.search(search);
 		
-		dic.search("apple");
-		dic.search("banana");
-		dic.search("grape");
-		dic.search("orange");
 	}
 
 }
--- kadai_021/src/kadai_021/Dictionary_Chapter21.java
+++ kadai_021/src/kadai_021/Dictionary_Chapter21.java
@@ -7,23 +7,26 @@ public class Dictionary_Chapter21 {
 	HashMap<String,String> dictionary = new HashMap<String,String>();
 	
 	public void tango() {
-	dictionary.put("apple","りんご");
-	dictionary.put("peach","桃");
-	dictionary.put("banana","バナナ");
-	dictionary.put("lemon","レモン");
-	dictionary.put("pear","梨");
-	dictionary.put("kiwi","キウイ");
-	dictionary.put("strawberry","いちご");
-	dictionary.put("grape","ぶどう");
-	dictionary.put("muscat","マスカット");
-	dictionary.put("cherry","さくらんぼ");
+		dictionary.put("apple","りんご");
+		dictionary.put("peach","桃");
+		dictionary.put("banana","バナナ");
+		dictionary.put("lemon","レモン");
+		dictionary.put("pear","梨");
+		dictionary.put("kiwi","キウイ");
+		dictionary.put("strawberry","いちご");
+		dictionary.put("grape","ぶどう");
+		dictionary.put("muscat","マスカット");
+		dictionary.put("cherry","さくらんぼ");
 	}
 	
-	public void search(String word) {
-		if(dictionary.get(word) == null ) {
-			System.out.println(word + "は存在しません");
-		}else {
-			System.out.println(word + "の意味は" + dictionary.get(word) + "です");
+	public void search(String[] word) {
+		for (int i = 0; i< word.length; i++) {
+			
+			if(dictionary.get(word[i]) == null ) {
+				System.out.println(word[i] + "は存在しません");
+			}else {
+				System.out.println(word[i] + "の意味は" + dictionary.get(word[i]) + "です");
+			}
 		}
 	}
 }
